acaf323d7fcfcf4dd003afea71ad35a676589983
Tatiana-Yatna <36164015+Tatiana-Yatna@users.noreply.github.com>
2018-02-16 19:35:54 +0200

    done

--- src/exercises/listMax.py
+++ src/exercises/listMax.py
@@ -1,10 +1,17 @@
 inputList = [45, 54, 87, 82, 56, 39, 32, 67, 77, 61]
 
-resultList = []
+#resultList = []
 
 '''Нужно написать программу, 
 которая в resultList помещает 3 
 максимальных значения из inputList и 
 выводит получившийся resultList в консоль 
 '''
+
+
+inputList.sort(reverse=True)
+i = 0
+while i<3:
+    i=i+1
+    print(inputList[i])
 
