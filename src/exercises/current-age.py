670c4c57e4863a846c2e8c67686d503dd322e75f
Tatiana-Yatna <36164015+Tatiana-Yatna@users.noreply.github.com>
2018-02-22 17:34:30 +0200

    is it was not so easy

--- src/exercises/current-age.py
+++ src/exercises/current-age.py
@@ -1,9 +1,12 @@
+import datetime
 born = int(input('What year were you born?'))
-year = int(input('What is it year now?'))
 
 
 def age(born):
-    return year - born
+    return datetime.datetime.now().year - born
 
 
 print(age(born))
+
+
+
